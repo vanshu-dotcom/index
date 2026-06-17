aee7ce3c2e6e615fcae20dbdb23428fafb9ab874
vanshu-dotcom <vanshuthakur24022009@gmail.com>
2026-06-17 09:18:45 +0000

    save this all

--- NEW JAVA/Practice2.js
+++ NEW JAVA/Practice2.js
@@ -1,21 +1,31 @@
-let age = 18;
-console.log(typeof age);
+// let age = 18;
+// console.log(typeof age);
 
-let score = "33";
+// let score = "33";
 
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 
-console.log(Number("33"));      // 33
-console.log(Number("33.5"));    // 33.5
-console.log(Number("vansh"));   // NaN
-console.log(Number("33abc"));   // NaN
-console.log(Number(true));      // 1
-console.log(Number(false));     // 0
-console.log(Number(null));      // 0
-console.log(Number(undefined)); // NaN
+// console.log(Number("33"));      // 33
+// console.log(Number("33.5"));    // 33.5
+// console.log(Number("vansh"));   // NaN
+// console.log(Number("33abc"));   // NaN
+// console.log(Number(true));      // 1
+// console.log(Number(false));     // 0
+// console.log(Number(null));      // 0
+// console.log(Number(undefined)); // NaN
 
+let isloggedIn = 1
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
