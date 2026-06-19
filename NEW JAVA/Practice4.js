d37359311bce0e8b5596f501e0b85b234031aa42
vanshu-dotcom <vanshuthakur24022009@gmail.com>
2026-06-19 06:52:29 +0000

    save

---
+++ NEW JAVA/Practice4.js
@@ -0,0 +1,33 @@
+//  Primetive
+
+//  7 types : string, number, Boolearn, Null, Undifined, Symbol, BigInt
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123') 
+
+// console.log(id === anotherId);
+
+
+const bigNumber =  34258664675858
+
+// Refrence (Non Primetive)
+
+// Array, Objects, Functions
+
+const heros =  ["shaktiman" , "naagraj", "doga"];
+let myObj = {
+    name: "vansh",
+    age: 17
+}
+const myfunction = function(){
+    console.log("Hello World")
+}
+console.log(typeof  bigNumber)
