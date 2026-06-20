8b5628ede3478b4be3c98f821d9364214f1eaa56
vanshu-dotcom <vanshuthakur24022009@gmail.com>
2026-06-20 10:09:28 +0000

    save too

--- NEW JAVA/Practice7.js
+++ NEW JAVA/Practice7.js
@@ -1,28 +1,28 @@
-// //Dates
+ Dates
 
-// // let myDate = new Date()
-// // console.log (myDate.toString());
-// // console.log (myDate.toDateString());
-// // console.log (myDate.toLocaleString());
-// // console.log (typeof myDate);
+  let myDate = new Date()
+  console.log (myDate.toString());
+  console.log (myDate.toDateString());
+ console.log (myDate.toLocaleString());
+  console.log (typeof myDate);
 
-// let date1 = new Date(2026, 2, 24);
-// let date2 = new Date(2026, 2, 24, 5, 3);
-// let date3 = new Date("2026-06-14");
-// let date4 = new Date("01-14-2026")
+ let date1 = new Date(2026, 2, 24);
+ let date2 = new Date(2026, 2, 24, 5, 3);
+ let date3 = new Date("2026-06-14");
+ let date4 = new Date("01-14-2026")
 
-// // console.log(date1.toLocaleString());
-// // console.log(date2.toLocaleString());
-// // console.log(date3.toLocaleString());
-// // console.log(date4.toLocaleString());
+  console.log(date1.toLocaleString());
+  console.log(date2.toLocaleString());
+  console.log(date3.toLocaleString());
+  console.log(date4.toLocaleString());
 
-// let myTimeStamp = Date.now()
+ let myTimeStamp = Date.now()
 
-// console.log(myTimeStamp);
-// console.log(console.log(date1.getTime()));
-// console.log(Math.floor(Date.now()/1000));
+ console.log(myTimeStamp);
+ console.log(console.log(date1.getTime()));
+ console.log(Math.floor(Date.now()/1000));
 
 
-let newDate = new Date()
+ let newDate = new Date()
 console.log(newDate.getMonth() + 1);
 console.log(newDate.getDay())
