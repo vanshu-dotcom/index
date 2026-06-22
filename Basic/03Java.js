16f0d43770d862e178dc6b9655960f4c8509c749
vanshu-dotcom <vanshuthakur24022009@gmail.com>
2026-06-22 08:47:12 +0000

    not complete

---
+++ Basic/03Java.js
@@ -0,0 +1,12 @@
+// object literals
+const JsUser = {
+    name: "Vansh",
+    age: 17,
+    location: "Dehli"
+    email: "vansh@gmail.com",
+    isloggedIn: false,
+    lastloginDays:  ["Monday","Saturday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser.[email])
