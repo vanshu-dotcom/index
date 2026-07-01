9369b0707c468c2d2a9386acc7deb93f5781692d
vanshu-dotcom <vanshuthakur24022009@gmail.com>
2026-07-01 09:58:29 +0000

    Arrow function

--- Basic2/Arrow.js
+++ Basic2/Arrow.js
@@ -3,8 +3,51 @@ const user = {
     price: 999,
 
     welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
+        // console.log(`${this.username} , welcome to website`);
+        console.log(this);
+        
     }
 }
 
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+// console.log(this);
+
+
+// function two() {
+//     let username = "vansh"
+//     // console.log(this.username)
+// }
+
+// two()
+
+// // const two = function () {
+// //     let username = "vansh"
+// //     console.log(this.username);
+// }
+
+// const two =  () => {
+//     let username = "vansh"
+//     // console.log(this.username);
+//     console.log(this);
+// }
+
+
+
+// two()
+
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) => (num1 + num2)
+const addTwo = (num1, num2) => ({username: "vansh"})
+
+console.log(addTwo(3,4))
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
